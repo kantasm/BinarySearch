927f888275320a3439555ac4c1950064ac209675
sunkanta <sunilmkanta@gmail.com>
2022-02-19 22:17:22 -0800

    Fixed the issues related to edge cases.

--- main.py
+++ main.py
@@ -21,6 +21,9 @@ def binary_search(num_list,num_to_find):
     return -1
 
 def binary_search_recursive(num_list,num_to_find,left_idx, right_idx):
+    if num_to_find < right_idx or num_to_find > left_idx:
+        return -1
+
     mid_idx = (left_idx + right_idx) // 2
     mid_num = num_list[mid_idx]
 
@@ -38,7 +41,8 @@ def binary_search_recursive(num_list,num_to_find,left_idx, right_idx):
 if __name__=='__main__':
 
     number_list=[i for i in range(10)]
-    find = 3
+    print(number_list)
+    find = 10
 
     idx = linear_search(number_list, find)
     print(f'Linear Search: Integer value found at index: {idx}')
@@ -46,5 +50,5 @@ if __name__=='__main__':
     idx = binary_search(number_list, find)
     print(f'Binary Search: Integer value found at index: {idx}')
 
-    idx = binary_search_recursive(number_list, find, len(number_list) - 1 , 0)
+    idx = binary_search_recursive(number_list, find, len(number_list) - 1, 0)
     print(f'Binary Search Recursive: Integer value found at index: {idx}')
